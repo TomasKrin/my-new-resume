562056bf923bc6ec28e2c5c104d6086f8d333a79
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-04-26 13:19:46 +0300

    changed layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,9 +1,11 @@
 import { PropsWithChildren } from "react";
+import TopBar from "../components/TopBar/TopBar";
 import styled from "styled-components";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <Wrapper>
+      <TopBar />
       <Container>
         <Content>{children}</Content>
       </Container>
@@ -13,13 +15,11 @@ const Layout = ({ children }: PropsWithChildren) => {
 
 export default Layout;
 
-const Wrapper = styled.div`
-  padding-top: 60px;
-  padding-bottom: 60px;
-  position: relative;
-`;
+const Wrapper = styled.div``;
 
 const Container = styled.div`
+  margin-top: 24px;
+  padding: 16px 36px;
   display: flex;
   position: relative;
 `;
